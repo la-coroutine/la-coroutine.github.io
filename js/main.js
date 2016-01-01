c8120167a9697301b3204682acee01f95d5bee65
Cecile Veneziani <contact@cecilitse.org>
2016-01-01 16:52:29 +0100

    Refactor CSS: use classes instead of ids

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
 $(document).ready(function(){
-    $('#navbar ul').localScroll({
-        hash:true,
-        duration:1200
-    });
-    $('#home #menu').localScroll({
-        hash:true,
-        duration:1200
-    });
+  $('.l-navbar ul').localScroll({
+    hash: true,
+    duration: 1200
+  });
 
-    //.parallax(xPosition, speedFactor, outerHeight) options:
-    //xPosition - Horizontal position of the element
-    //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
-    //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-    $('#home').parallax("50%", 0.1);
-    $('#espace').parallax("50%", 0.1);
-    $('#coworking').parallax("50%", 0.6);
-    $('#coworking .bg').parallax("50%", 0.9);
-    $('#tierslieu').parallax("50%", 0.1);
-    $('#tierslieu .bg').parallax("57%", 0.6);
+  $('.m-home--banners--menu').localScroll({
+    hash: true,
+    duration: 1200
+  });
 
-    $('#navbar ul a').click(function(){
-        $('#navbar li').removeClass('active');
-        $(this).parent('li').addClass('active');
-    });
+  //.parallax(xPosition, speedFactor, outerHeight) options:
+  //xPosition - Horizontal position of the element
+  //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+  //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+  $('.m-home--banners').parallax("50%", 0.1);
+  $('.m-home--space').parallax("50%", 0.1);
+  $('.m-home--coworking').parallax("50%", 0.6);
+  $('.m-home--coworking .bg').parallax("50%", 0.9);
+  $('.m-home--third-place').parallax("50%", 0.1);
+  $('.m-home--third-place .bg').parallax("57%", 0.6);
 
-    $('#video-home').videoBG({
-        webm:'assets/header.webm',
-        poster:'img/video_coco.jpg',
-        scale:true,
-        zIndex:0
-    });
+  $('.l-navbar ul a').click(function(){
+    $('.l-navbar li').removeClass('active');
+    $(this).parent('li').addClass('active');
+  });
+
+  $('.m-home--banners--video').videoBG({
+    webm: 'assets/header.webm',
+    poster: 'img/video_coco.jpg',
+    scale: true,
+    zIndex: 0
+  });
 });
